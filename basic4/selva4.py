67adec6852704a67fc8c299598e3169e095287ad
selvamnair <36755442+selvamnair@users.noreply.github.com>
2018-02-23 14:42:43 +0530

    Create selva4.py

---
+++ basic4/selva4.py
@@ -0,0 +1,5 @@
+x=input("enter the letter ")
+if x in('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'):
+  print("is alphabet")
+else:
+  print("not alphabet")
